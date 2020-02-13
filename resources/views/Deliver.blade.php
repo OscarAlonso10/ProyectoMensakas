d9554d2d0daa669a9af8b25716d80935f291f930
Pradas137 <pradas137@gmail.com>
2020-02-13 08:29:23 +0100

    nuevo

--- resources/views/Deliver.blade.php
+++ resources/views/Deliver.blade.php
@@ -7,21 +7,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
 </head>
-<body>
-  <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/layout') }}">layout</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+<body> 
     <div class="container">
   <h1>AdminPanel:Deliver</h1>
   <div class="btn-group-vertical">
--- resources/views/Menu.blade.php
+++ resources/views/Menu.blade.php
@@ -8,20 +8,6 @@
 
 </head>
 <body>
-  <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/layout') }}">layout</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
     <div class="container">
   <h1>AdminPanel:Menu</h1>
   <div class="btn-group-vertical">

7f7c7efcf2d9ea65fa9125a9998baa23c9c21590
Pradas137 <pradas137@gmail.com>
2020-02-17 20:29:13 +0100

    consumerfrom

--- resources/views/loginConsumer.blade.php
+++ resources/views/loginConsumer.blade.php
@@ -1,55 +1,75 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="css/bsvalidate/style.css">
-</head>
-<body>
-    <div class="container">
-       <div id="demo-of-custom-msgs" class="section">
-        <h2>Consumer</h2>
-        <div>
-          <div>
-            <div id="customMessagesExampleDemo">
-              <form id="customMessagesForm" action="saveinfo.aspx" method="post" class="well">
-                <div class="form-group">
-                  <label class="control-label">Enter Your Name</label>
-                  <input type="text" name="name" class="form-control" />
-                </div>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
 
-                <div class="form-group">
-                  <label class="control-label">Enter Email</label>
-                  <input type="text" name="email" class="form-control" />
-                </div>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-12 col-md-8 col-lg-6 pb-5">
 
-                <div class="form-group">
-                  <label class="control-label">Please Confirm Email</label>
-                  <input type="text" name="emailConfirm" class="form-control" />
-                </div>
 
-                <div class="form-group">
-                  <label class="control-label">location</label>
-                  <input type="text" name="location" class="form-control" />
-                </div>
+                    <!--Form with header-->
+                    <form action="businessClient" method="post">
+                        <div class="card border-primary rounded-0">
+                            <div class="card-header p-0">
+                                <div class="bg-info text-white text-center py-2">
+                                    <h3><i class="fa fa-user-circle"></i>Mensakas</h3>
+                                    <p class="m-0">Consumer</p>
+                                </div>
+                            </div>
+                            <div class="card-body p-3">
 
-                <div class="form-group">
-                  <label class="control-label">Phone</label>
-                  <input type="tel" name="phone" class="form-control" />
-                </div>
+                                <!--Body-->
+                                <div class="form-group">
+                                    <div class="input-group mb-2">
+                                        <div class="input-group-prepend">
+                                            <div class="input-group-text"><i class="fa fa-user"></i></div>
+                                        </div>
+                                        <input type="text" class="form-control" id="nombre" name="nombre" placeholder="First name" required>
+                                    </div>
+                                    <div class="input-group mb-2">
+                                        <div class="input-group-prepend">
+                                            <div class="input-group-text"><i class="fa fa-user"></i></div>
+                                        </div>
+                                        <input type="text" class="form-control" id="last_name" name="last_name" placeholder="Last name" required>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="input-group mb-2">
+                                        <div class="input-group-prepend">
+                                            <div class="input-group-text"><i class="fa fa-envelope"></i></div>
+                                        </div>
+                                        <input type="email" class="form-control" id="nombre" name="email" placeholder="ejemplo@gmail.com" required>
+                                    </div>
+                                </div>
 
-                <div class="form-group">
-                  <input type="submit" class="btn btn-danger" value="Save Info" />
+                                <div class="form-group">
+                                    <div class="input-group mb-2">
+                                        <div class="input-group-prepend">
+                                            <div class="input-group-text"><i class="fa fa-phone"></i></div>
+                                        </div>
+                                        <input type="tel" class="form-control" id="phone" name="phone" placeholder="123-4567-8901" required>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <div class="input-group mb-2">
+                                        <div class="input-group-prepend">
+                                            <div class="input-group-text"><i class="fa fa-map-marker"></i></div>
+                                        </div>
+                                        <input type="text" class="form-control" id="location" name="location" placeholder="location" required>
+                                    </div>
+                                </div>
+                                
+                                <div class="text-center">
+                                    <input type="submit" value="Enviar" class="btn btn-info btn-block rounded-0 py-2">
+                                </div>
+                            </div>
+
+                        </div>
+                    </form>
+                    <!--Form with header-->
                 </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="js/bsvalidate/jquery.bsvalidate.js"></script>
-    <script type="text/javascript" src="js/bsvalidate/main.js"></script>
-</body>
-</html>
-
+</div>

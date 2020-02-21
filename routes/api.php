38fc4daabd1f11476e29f738144f669d797c1c86
Pradas137 <pradas137@gmail.com>
2020-02-21 17:30:09 +0100

    API

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
-
+use App\Business;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,4 +15,10 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::get('/business', function () {
+	$business = Business::all();
+    return $business->toJson();
 });

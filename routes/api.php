82cdc69923a00bedf1d02a504ea516a7df735189
Oscar Alonso <oskralonso10@gmail.com>
2020-02-28 16:21:18 +0100

    apilar

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Http\Request;
-
+use App\Business;
+use App\Product;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,4 +16,15 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::get('/business', function () {
+	$business = Business::all()->toJson();
+    return ;
+});
+97724
+Route::get('/product/{idBusiness}', function ($idBusiness) {
+	$product = Product::where('fk_business_id','=', $idBusiness)->get();
+    return $product->toJson();
 });
